97bfcf8f9cd1ce81f66b2b353ca7d67859841a11
Mathias <hmvege@ulrik.uio.no>
2017-07-03 23:06:03 +0200

    Fixed up some of the text reading. Rudimentary plot. Will check tomorrow if correct

--- ex1ect.py
+++ ex1ect.py
@@ -1,62 +1,31 @@
 
 #First exercise ECT
 
-import numpy 
+import numpy as np, matplotlib.pyplot as plt
 
 #Upload data from aud16
 
-aud16 = numpy.loadtxt("aud16.dat", skiprows=1)
+aud16 = np.loadtxt("../nushellx/toi/mass-data/aud16.dat", skiprows=2)#, usecols=(0,1,2,3,4,5))
 
-ia = []
-iz = []
-BE = []
-error = []
-i_n = []
-
-for i in range(0,len(aud16)): 
-    iz.append(aud16[i][0])
-    ia.append(aud16[i][1])
-    BE.append(aud16[i][2])
-    error.append(aud16[i][3])
-    i_n.append(aud16[i][5])
-
+ia = aud16[:,0]
+iz = aud16[:,1]
+BE = aud16[:,2]
+error = aud16[:,3]
+i_n = aud16[:,4]
 
 #Upload data from rms13
 
-rms13 = numpy.loadtxt("rms13.dat", skiprows=2, usecols=(0,1,2,3,4))
+rms13 = np.loadtxt("../nushellx/toi/mass-data/rms13.dat", skiprows=2, usecols=(0,1,2,3,4))
 
-z = []
-n = []
-a = []
-rms = []
-error = []
+z = rms13[:,0]
+n = rms13[:,1]
+a = rms13[:,2]
+rms = rms13[:,3]
+error = rms13[:,4]
 
-for i in range(0,len(rms13)):
-   z.append(rms13[i][0])  
-   n.append(rms13[i][1])  
-   a.append(rms13[i][2])  
-   rms.append(rms13[i][3])  
-   error.append(rms13[i][4])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Plotting the variations
+plt.errorbar(z, rms, yerr=error, fmt=".", ecolor="r", label="rms")
+plt.xlabel(r"$Z$",fontsize=20)
+plt.ylabel(r"$\bar{r}_s$",fontsize=20)
+plt.legend()
+plt.show()
