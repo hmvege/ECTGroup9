5386d86fd6e2f36582c6c027181306a5e489c7bb
amromero <amr560@york.ac.uk>
2017-07-13 13:07:59 +0100

    shell with sd shell with any number of particles

--- shellcode/shell.py
+++ shellcode/shell.py
@@ -8,13 +8,14 @@ import itertools
 
 #sp_states = np.loadtxt("{}".format(filename), skiprows=1)
 
-sp_states = np.loadtxt("inputsp3.txt", skiprows=1)
+sp_states = np.loadtxt("usdbint.txt", skiprows=1)
 
 Index_inp = []
 n_inp = []
 l_inp = []
 tj_inp = []
 tm_inp = []
+sp_energies = []
 
 
 
@@ -24,6 +25,7 @@ for i in range(0,len(sp_states)):
     l_inp.append(sp_states[i][2])
     tj_inp.append(sp_states[i][3])
     tm_inp.append(sp_states[i][4])
+    sp_energies.append(sp_states[i][5])
 
 
 Index_inp = [int(i) for i in Index_inp]
@@ -32,8 +34,6 @@ m_inp = [0.5 * j for j in tm_inp]
 
 
 #Number of sp states
-
-
 N_sp = len(n_inp)
 
 #Matrix to store the M scheme
@@ -49,12 +49,14 @@ N_sp = len(n_inp)
 #print "M SCHEME "
 
 
-
-
 #Specify the number of particles in our space
 
  
 N_part = 4 #int(raw_input("enter the number of particles \n"))
+
+if (N_part > N_sp):
+  print "FATAL ERROR: NUMBER OF PARTICLES IS GREATER THAN THE NUMBER OF SP STATES. PROGRAM STOPS"
+  quit()
 
 #Construct all the Slater determinants 
 
@@ -77,17 +79,6 @@ SD_M_fix = []   #Slater Determinants with fixed M
 
 #Taking into account only sum of m
 
-#for i in SD:
-#  m_tot_SD = 0
-#  for j in xrange(N_part):
-#     m_tot_SD = m_tot_SD + m_inp[Index_inp.index(int(i[j]))]
-#  if (m_tot_SD == M_fix):
-#    SD_M_fix.append(i)
-
-#Taking into account also different n
-
-
-
 for i in SD:
   m_tot_SD = 0
   for j in xrange(N_part):
@@ -105,38 +96,73 @@ print
 
 SD_M_fix_new = []
 
+#Taking into account also different n
 
-#### We can only include states with same n, here we retrieve states with the same n from all the SD with M=0
+#### We can only include states with same n, here we retrieve states with the same (n,l,j) from all the SD with M=0
 
 if (N_part % 2 == 0):
 
+ #for i in SD_M_fix: 
+ # for j in range(N_part/2):
+ #   if (n_inp[Index_inp.index(np.split(np.asarray(i),2)[j][0])] == n_inp[Index_inp.index(np.split(np.asarray(i),2)[j][1])] and 
+ #       l_inp[Index_inp.index(np.split(np.asarray(i),2)[j][0])] == l_inp[Index_inp.index(np.split(np.asarray(i),2)[j][1])] and
+ #       j_inp[Index_inp.index(np.split(np.asarray(i),2)[j][0])] == j_inp[Index_inp.index(np.split(np.asarray(i),2)[j][1])]):
+ #           SD_M_fix_new.append(np.split(np.asarray(i),2)[j])
+
+
  for i in SD_M_fix: 
-  #print np.split(np.asarray(i),2)
   for j in range(N_part/2):
-    if (n_inp[Index_inp.index(np.split(np.asarray(i),2)[j][0])] == n_inp[Index_inp.index(np.split(np.asarray(i),2)[j][1])]):
-      SD_M_fix_new.append(np.split(np.asarray(i),2)[j])
+    if (n_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][0])] == n_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][1])] and 
+        l_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][0])] == l_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][1])] and
+        j_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][0])] == j_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][1])]):
+               SD_M_fix_new.append(np.split(np.asarray(i),N_part/2)[j])
+
+
+ #print SD_M_fix_new
 
  SD_M_fix_new = [tuple(i) for i in SD_M_fix_new]
 
  SD_M_fix_new = list(set(SD_M_fix_new))
 
+ 
+  ####### WE HAVE TO JOIN THE PAIRS IN NPART/2 IN GENERAL WAY!!!!
+
+
+ SD_trial = list(itertools.combinations(SD_M_fix_new,N_part/2))  #All possible combinations of pairs with same quantum numbers
+
+ SD_trial = [[j for j in i] for i in SD_trial]
+
+ SD_trial2 = []
+
+ for k in range(len(SD_trial)):
+  SD_trial2.append(tuple(j for i in SD_trial[k] for j in (i if isinstance(i, tuple) else (i,))))
+
+ SD_trial2.sort()
 
  SD_M_fix_new1 = []
 
- for i in range(len(SD_M_fix_new)):
-  for j in range(i+1,len(SD_M_fix_new)):
-      SD_M_fix_new1.append(tuple(sorted(list(SD_M_fix_new[i]) + list(SD_M_fix_new[j]))))
+ for i in SD_trial2:
+  if (tuple(sorted(i)) in SD_M_fix):
+    SD_M_fix_new1.append(tuple(sorted(i)))
 
  SD_M_fix_new1.sort()
+ 
 
- #print SD_M_fix_new1
+ SD_M_fix = list(set(SD_M_fix_new1))
 
- SD_M_fix = SD_M_fix_new1
-
- print SD_M_fix 
+ SD_M_fix.sort()
+ 
+ print 
+ print "CORRECT SD WITH M FIXED AND GOOD QUANTUM NUMBERS FOR PAIRS"
+ print 
+ print SD_M_fix
+ print 
+ print "THERE ARE ", len(SD_M_fix) 
 
 
 ###################  
+
+# Functions to compute the matrix elements
   
 
 def Update_SD(x,p,q,s,r):
@@ -172,7 +198,9 @@ def Check_SD(x):
 
 SD_M_fix = [list(i) for i in SD_M_fix]
 
+##############################################################################
 
+#### EXAMPLES IN FCI MINTED FOR DIFFERENT INPUT OF SP STATES. CAREFUL, YOU MAY HAVE TO MODIFY THE INPUT CODE TO MAKE IT WORK
 
 #Build the hamiltonian matrix
 
@@ -252,12 +280,21 @@ delta = 1.0  #Strength one body operator
 
 #print Hmat
 
+###################################################################################
+
+
+###################################################################################
+
+#        SHELL MODEL PROJECT ON SD SHELL WITH A PAIR BASIS STARTS HERE!!!!
+
+
+####################################################################################
 
 # Build hamiltonian matrix with pairing Hamiltonian FCI-minted pag 29
 
 Hmat = np.zeros((len(SD_M_fix),len(SD_M_fix)))   #Initialize  
 
-g = 1.0   #Strength 
+g = 0.0   #Strength 
 
 for p in range(len(Index_inp)): 
  for q in range(p+1, len(Index_inp)):
@@ -267,35 +304,119 @@ for p in range(len(Index_inp)):
         if (Check_SD(Update_SD(SD_M_fix[i],p+1,q+1,r+1,s+1))[0] == 1):
                Hmat[(i,Check_SD(Update_SD(SD_M_fix[i],p+1,q+1,r+1,s+1))[1])] = -g
 
+#There is a factor of two in the diagonal from the two body operator 
+for i in range(len(SD_M_fix)):
+  Hmat[(i,i)] += -g 
 
-print SD_M_fix
+
 
 print
 print 
 print 
-print Hmat
+#print Hmat
 print 
 print 
 print 
 
 #Include one body operator term in the hamiltonian matrix
 
-delta = 0.0  #Strength one body operator
+delta = 1.0  #Strength one body operator
+
+#for i in range(len(SD_M_fix)):
+#  for j in Index_inp:
+#    if (j in SD_M_fix[i]):
+     #  print i,j
+#        Hmat[(i,i)] += delta * (i+1) / 2.0  
+
+#Include the sp energies in the hamiltonian matrix from the one body operator (diagonal)
 
 for i in range(len(SD_M_fix)):
-  for j in Index_inp:
-    if (j in SD_M_fix[i]):
-     #  print i,j
-        Hmat[(i,i)] += delta * (i+1) / 2.0
+  for j in range(N_part):
+    Hmat[(i,i)] += sp_energies[Index_inp.index(SD_M_fix[i][j])]        
+
+
+
 
 
 print Hmat
     
-eigVal,eigVec = np.linalg.eig(Hmat)
+eigVal,eigVec = np.linalg.eigh(Hmat)
 
 print eigVal
 
-print eigVec
+#print eigVec
+
+np.savetxt("Hmatrix.txt", Hmat, fmt="%.5f")
+
+
+
+
+
+## Counting permutations in sorting 
+
+#print SD_M_fix[0]
+
+l = [1, 2, 3, 5, 4, 6]
+
+def perm_sorted(x):
+  s = 1
+  for item in x:
+    if item == s:
+      s += 1
+  return len(x) - s + 1
+
+
+s = 1
+for item in SD_M_fix[0]:
+  if item == s:
+    s += 1
+#print len(SD_M_fix[0]) - s + 1  
+
+
+###COMPUTE SD WITHOUT SORTING
+
+def Update_SD1(x,p,q,s,r):
+ #Update SD (x) when we apply the interaction operator ap+ aq+ as ar
+  SD_new = x[:]
+  perms = []
+  for i in range(len(x)): 
+   for j in range(i+1,len(x)):
+     SD_rem = []
+     if ((x[i] == s and x[j] == r) or (x[j] == s and x[i] == r)):
+      SD_rem.append(s)
+      SD_rem.append(r)
+      SD_new = list(set(SD_new) ^ set(SD_rem))
+
+  for i in SD_new: 
+    if (i == q or i == p):
+      SD_new = []
+  SD_new.insert(0,q)
+  SD_new.insert(0,p)
+  #SD_new.sort()
+  
+  if (len(SD_new) != len(x)):
+    SD_new = []
+  
+  return SD_new
+
+def Check_SD1(x):
+  for i in SD_M_fix:
+    if (x == i):
+      return [1, SD_M_fix.index(i) ]
+      break 
+  return [0 ,SD_M_fix.index(i) ]
+
+
+
+#print SD_M_fix[0]
+
+#print Update_SD1(SD_M_fix[0],1,4,1,4)
+
+#print perm_sorted(Update_SD1(SD_M_fix[0],1,4,1,4))
+
+#print Update_SD1(SD_M_fix[0],1,4,1,4).sort()
+
+#print perm_sorted(Update_SD1(SD_M_fix[0],1,4,1,4))
 
 
 
