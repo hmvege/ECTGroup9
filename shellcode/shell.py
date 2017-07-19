a8f707c448830e8aada81848ec3d0fa136ac00c8
amromero <amr560@york.ac.uk>
2017-07-19 13:53:41 +0100

    Final code

--- shellcode/shell.py
+++ shellcode/shell.py
@@ -9,6 +9,7 @@ import itertools
 #sp_states = np.loadtxt("{}".format(filename), skiprows=1)
 
 sp_states = np.loadtxt("usdbint.txt", skiprows=1)
+mels = np.loadtxt("melsusdbint.txt", skiprows=1)
 
 Index_inp = []
 n_inp = []
@@ -16,6 +17,8 @@ l_inp = []
 tj_inp = []
 tm_inp = []
 sp_energies = []
+
+pqrs = []
 
 
 
@@ -26,6 +29,11 @@ for i in range(0,len(sp_states)):
     tj_inp.append(sp_states[i][3])
     tm_inp.append(sp_states[i][4])
     sp_energies.append(sp_states[i][5])
+
+for i in range(0, len(mels)):
+   pqrs.append((mels[i][0], mels[i][1], mels[i][2], mels[i][3], mels[i][4]))
+
+
 
 
 Index_inp = [int(i) for i in Index_inp]
@@ -52,7 +60,7 @@ N_sp = len(n_inp)
 #Specify the number of particles in our space
 
  
-N_part = 4 #int(raw_input("enter the number of particles \n"))
+N_part = int(raw_input("enter the number of particles \n"))
 
 if (N_part > N_sp):
   print "FATAL ERROR: NUMBER OF PARTICLES IS GREATER THAN THE NUMBER OF SP STATES. PROGRAM STOPS"
@@ -73,18 +81,19 @@ print
 
 #Set up M-scheme
 
-M_fix = 0 #float(raw_input("enter M \n"))  #Fix total M
+#M_fix = 0 #float(raw_input("enter M \n"))  #Fix total M
 
 SD_M_fix = []   #Slater Determinants with fixed M 
 
 #Taking into account only sum of m
 
-for i in SD:
-  m_tot_SD = 0
-  for j in xrange(N_part):
-    m_tot_SD = m_tot_SD + m_inp[Index_inp.index(int(i[j]))]
-  if (m_tot_SD == M_fix):
-    SD_M_fix.append(i)
+for M_fix in range(-8,8):
+ for i in SD:
+   m_tot_SD = 0
+   for j in xrange(N_part):
+     m_tot_SD = m_tot_SD + m_inp[Index_inp.index(int(i[j]))]
+   if (m_tot_SD == M_fix):
+     SD_M_fix.append(i)
 
 
 print SD_M_fix
@@ -112,9 +121,6 @@ if (N_part % 2 == 0):
 
  for i in SD_M_fix: 
   for j in range(N_part/2):
-    if (n_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][0])] == n_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][1])] and 
-        l_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][0])] == l_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][1])] and
-        j_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][0])] == j_inp[Index_inp.index(np.split(np.asarray(i),N_part/2)[j][1])]):
                SD_M_fix_new.append(np.split(np.asarray(i),N_part/2)[j])
 
 
@@ -175,6 +181,13 @@ def Update_SD(x,p,q,s,r):
       SD_rem.append(s)
       SD_rem.append(r)
       SD_new = list(set(SD_new) ^ set(SD_rem))
+     
+
+  if (SD_new == []):
+    return SD_new
+      
+
+   
 
   for i in SD_new: 
     if (i == q or i == p):
@@ -188,12 +201,55 @@ def Update_SD(x,p,q,s,r):
   
   return SD_new
 
+def Update_SD1(x,p,q,s,r):
+ #Update SD (x) when we apply the interaction operator ap+ aq+ as ar
+  SD_new = x[:]
+  perms = []
+  for i in range(len(x)): 
+   for j in range(i+1,len(x)):
+     if ((x[i] == s and x[j] == r) or (x[j] == s and x[i] == r)):
+       SD_new[i], SD_new[j] = 0,-1
+  
+  if (SD_new == x):
+    SD_new = []
+    return SD_new
+
+  for i in range(len(x)):
+    if (SD_new[i] == q or SD_new[i] == p):
+      SD_new = []
+      break 
+    elif (SD_new[i] == 0):
+      SD_new[i] = p
+    elif (SD_new[i] == -1):
+      SD_new[i] = q
+  
+  return SD_new
+
 def Check_SD(x):
   for i in SD_M_fix:
     if (x == i):
       return [1, SD_M_fix.index(i) ]
       break 
   return [0 ,SD_M_fix.index(i) ]
+
+def perm_sorted2(x):
+  s = 0 
+  sortflag = False
+
+  while not sortflag: 
+    sortflag = True 
+    for i in range(len(x)-1): 
+      if (x[i] > x[i+1]): 
+        sortflag = False 
+        s = s + 1 
+        x[i], x[i+1] = x[i+1], x[i]
+
+  if (s == 0):
+   return True
+  elif (s % 2 == 0):
+   return True 
+  else: 
+   return False
 
 
 SD_M_fix = [list(i) for i in SD_M_fix]
@@ -288,25 +344,58 @@ delta = 1.0  #Strength one body operator
 #        SHELL MODEL PROJECT ON SD SHELL WITH A PAIR BASIS STARTS HERE!!!!
 
 
-####################################################################################
+###################################################################################
+
 
 # Build hamiltonian matrix with pairing Hamiltonian FCI-minted pag 29
 
 Hmat = np.zeros((len(SD_M_fix),len(SD_M_fix)))   #Initialize  
 
-g = 0.0   #Strength 
+g = 1.0   #Strength 
 
-for p in range(len(Index_inp)): 
- for q in range(p+1, len(Index_inp)):
-   for s in range(len(Index_inp)): 
-    for r in range(s+1,len(Index_inp)):
-      for i in range(len(SD_M_fix)):
-        if (Check_SD(Update_SD(SD_M_fix[i],p+1,q+1,r+1,s+1))[0] == 1):
-               Hmat[(i,Check_SD(Update_SD(SD_M_fix[i],p+1,q+1,r+1,s+1))[1])] = -g
+scale_factor = (18.0/(16+N_part)) ** 0.3
+
+#for p in range(len(Index_inp)): 
+# for q in range(p+1, len(Index_inp)):
+#   for s in range(len(Index_inp)): 
+#    for r in range(s+1,len(Index_inp)):
+#      for i in range(len(SD_M_fix)):
+#        if (Check_SD(Update_SD(SD_M_fix[i],p+1,q+1,r+1,s+1))[0] == 1):
+#               Hmat[(i,Check_SD(Update_SD(SD_M_fix[i],p+1,q+1,r+1,s+1))[1])] = -g
+
+
+#Hamiltonian matrix including the matrix elements from USDB interaction 
+
+
+for i in range(len(SD_M_fix)):
+  updatesort = 0
+  for j in pqrs:
+    updatesort = sorted(Update_SD1(SD_M_fix[i],j[0],j[1],j[3],j[2]))
+    if (Check_SD(updatesort)[0] == 1):
+     if (perm_sorted2(Update_SD1(SD_M_fix[i],j[0],j[1],j[3],j[2])) == True):
+   #   Hmat[(i,Check_SD(Update_SD1(SD_M_fix[i],j[0],j[1],j[3],j[2]))[1])] += g * j[4]
+       Hmat[(i,Check_SD(updatesort)[1])] += g * j[4]
+     # if (i != Check_SD(Update_SD1(SD_M_fix[i],j[0],j[1],j[3],j[2]))[1]):
+     #  Hmat[(Check_SD(Update_SD1(SD_M_fix[i],j[0],j[1],j[3],j[2]))[1], i)] = Hmat[(i,Check_SD(Update_SD(SD_M_fix[i],j[0],j[1],j[3],j[2]))[1])]
+     elif (perm_sorted2(Update_SD1(SD_M_fix[i],j[0],j[1],j[3],j[2])) == False): 
+   #   Hmat[(i,Check_SD(Update_SD1(SD_M_fix[i],j[0],j[1],j[3],j[2]))[1])] -= g * j[4]
+       Hmat[(i,Check_SD(updatesort)[1])] -= g * j[4]
+     # if (i != Check_SD(Update_SD1(SD_M_fix[i],j[0],j[1],j[3],j[2]))[1]):
+     #  Hmat[(Check_SD(Update_SD1(SD_M_fix[i],j[0],j[1],j[3],j[2]))[1], i)] = Hmat[(i,Check_SD(Update_SD(SD_M_fix[i],j[0],j[1],j[3],j[2]))[1])]
+       
+
+
+
+
+for i in range(len(SD_M_fix)):
+ for j in range(len(SD_M_fix)):
+   if (i != j and Hmat[i,j] != 0):
+      Hmat[j,i] = Hmat[i,j]
+
 
 #There is a factor of two in the diagonal from the two body operator 
-for i in range(len(SD_M_fix)):
-  Hmat[(i,i)] += -g 
+#for i in range(len(SD_M_fix)):
+#  Hmat[(i,i)] = 2 * Hmat[(i,i)] 
 
 
 
@@ -325,29 +414,71 @@ delta = 1.0  #Strength one body operator
 #for i in range(len(SD_M_fix)):
 #  for j in Index_inp:
 #    if (j in SD_M_fix[i]):
-     #  print i,j
+#    #  print i,j
 #        Hmat[(i,i)] += delta * (i+1) / 2.0  
 
 #Include the sp energies in the hamiltonian matrix from the one body operator (diagonal)
+
 
 for i in range(len(SD_M_fix)):
   for j in range(N_part):
     Hmat[(i,i)] += sp_energies[Index_inp.index(SD_M_fix[i][j])]        
 
 
+#Check if hamiltonian matrix is symmetric
+print 
+print "Is Hmat symmetric?"
+print
+print np.allclose(Hmat, Hmat.T)
+print 
 
 
-
-print Hmat
+#print Hmat
     
 eigVal,eigVec = np.linalg.eigh(Hmat)
 
+print 
+print "Eigenvalues: "
+print
+print 
 print eigVal
+
+
+####PROBLEM WITH THE PHASE. IT DOESNT WORK
 
 #print eigVec
 
-np.savetxt("Hmatrix.txt", Hmat, fmt="%.5f")
+np.savetxt("Hmatrix.txt", Hmat, fmt="%.2f")
 
+np.savetxt("Energies.txt", eigVal, fmt="%.4f")
+
+
+#print SD_M_fix[5]
+#print pqrs[2]
+#print Update_SD1(SD_M_fix[5],pqrs[2][0],pqrs[2][1],pqrs[2][3],pqrs[2][2])
+
+#print sorted(Update_SD1(SD_M_fix[5],pqrs[2][0],pqrs[2][1],pqrs[2][3],pqrs[2][2]))
+
+#print Check_SD(Update_SD1(SD_M_fix[5],pqrs[2][0],pqrs[2][1],pqrs[2][3],pqrs[2][2]))
+
+#print Check_SD(sorted(Update_SD1(SD_M_fix[5],pqrs[2][0],pqrs[2][1],pqrs[2][3],pqrs[2][2])))
+
+
+#updatesort = 0
+
+#print "FINAL " 
+#print SD_M_fix[5]
+#for j in pqrs:
+#  updatesort = sorted(Update_SD1(SD_M_fix[5],j[0],j[1],j[3],j[2]))
+#  if (Check_SD(updatesort)[0] == 1):
+#   if (perm_sorted2(Update_SD1(SD_M_fix[5],j[0],j[1],j[3],j[2])) == True):
+#    print Update_SD1(SD_M_fix[5],j[0],j[1],j[3],j[2])
+#    print updatesort
+#    print j 
+#    print Check_SD(Update_SD1(SD_M_fix[5],j[0],j[1],j[3],j[2]))
+
+
+#print Check_SD(Update_SD1(SD_M_fix[5],j[0],j[1],j[3],j[2]))
 
 
 
@@ -366,6 +497,35 @@ def perm_sorted(x):
   return len(x) - s + 1
 
 
+def perm_sorted1(x):
+  s = 0
+  t = 0
+  for i in range(len(x)-1): 
+    if (x[i] > x[i+1]):
+      t = x[i+1] 
+      x[i+1] = x[i]
+      x[i] = t
+      s = s + 1
+  return s 
+  return x 
+
+#def perm_sorted2(x):
+#  s = 0 
+#  sortflag = False
+
+#  while not sortflag: 
+#    sortflag = True 
+#    for i in range(len(x)-1): 
+#      if (x[i] > x[i+1]): 
+#        sortflag = False 
+#        s = s + 1 
+#        x[i], x[i+1] = x[i+1], x[i]
+#  if (s % 0 == 0):
+#   return True
+#  else: 
+#   return False
+
+
 s = 1
 for item in SD_M_fix[0]:
   if item == s:
@@ -381,21 +541,21 @@ def Update_SD1(x,p,q,s,r):
   perms = []
   for i in range(len(x)): 
    for j in range(i+1,len(x)):
-     SD_rem = []
      if ((x[i] == s and x[j] == r) or (x[j] == s and x[i] == r)):
-      SD_rem.append(s)
-      SD_rem.append(r)
-      SD_new = list(set(SD_new) ^ set(SD_rem))
-
-  for i in SD_new: 
-    if (i == q or i == p):
-      SD_new = []
-  SD_new.insert(0,q)
-  SD_new.insert(0,p)
-  #SD_new.sort()
+       SD_new[i], SD_new[j] = 0,-1
   
-  if (len(SD_new) != len(x)):
+  if (SD_new == x):
     SD_new = []
+    return SD_new
+
+  for i in range(len(x)):
+    if (SD_new[i] == q or SD_new[i] == p):
+      SD_new = []
+      break 
+    elif (SD_new[i] == 0):
+      SD_new[i] = p
+    elif (SD_new[i] == -1):
+      SD_new[i] = q
   
   return SD_new
 
@@ -408,15 +568,12 @@ def Check_SD1(x):
 
 
 
-#print SD_M_fix[0]
+#print Update_SD1([1,4,3,2],8,9,3,2)
 
-#print Update_SD1(SD_M_fix[0],1,4,1,4)
+#print perm_sorted2([1,4,8,9])
 
-#print perm_sorted(Update_SD1(SD_M_fix[0],1,4,1,4))
 
-#print Update_SD1(SD_M_fix[0],1,4,1,4).sort()
 
-#print perm_sorted(Update_SD1(SD_M_fix[0],1,4,1,4))
 
 
 
